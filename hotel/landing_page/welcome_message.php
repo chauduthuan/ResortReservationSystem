05ab4a8bdd6527b574f2f9f218033ef102b24951
alex giang <alex giang>
2017-11-19 17:09:27 -0800

    Added comment about slideshow code adapted from w2schools.com

--- hotel/landing_page/welcome_message.php
+++ hotel/landing_page/welcome_message.php
@@ -15,6 +15,8 @@
                   
                 </div>
                 <!-- /widget-content --> 
+                <!-- SLIDESHOW adapted from https://www.w3schools.com/howto/howto_js_slideshow.asp -->  
+
                 <!DOCTYPE html>
 <html>
 <head>
