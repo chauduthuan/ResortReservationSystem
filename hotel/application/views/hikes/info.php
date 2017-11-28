2729995c670f15a9167dde306af7625b5e08ea08
alex giang <alex giang>
2017-11-27 22:09:08 -0800

    added information to info.php for hikes and restaraunts

--- hotel/application/views/hikes/info.php
+++ hotel/application/views/hikes/info.php
@@ -1,9 +1,27 @@
 <div class="main">
-  <div class="main-inner">
-    <div class="container">
-      <div class="span4">
+    <div class="main-inner">
+        <div class="container">
+            <div class="span4">
+		        <div class="widget widget-nopad" id="target-1">
+            		<div class="widget-header"> <i class="icon-list-alt"></i>
+             			<h3> Hiking and Rentals</h3>
+            		</div>
+            		<!-- /widget-header -->
+            		<div class="widget-content">
+             			<div class="widget big-stats-container">
+                			<div class="widget-content">
+                 				<h6 class="bigstats">  Here are the different tails you can go on:</h6>
+                 					Information to be added and formatted
 
-      </div>
+                 				<h6 class="bigstats">  Here are all types of equipment you can rent:</h6>
+                 					Information to be added and formatted
+                 				</div>
+                 			</div>
+                 		</div>
+                 	</div>
+                </div>
+                <!-- widget-content -->
+      		</div>
+  		</div>
   	</div>
-  </div>
 </div>
--- hotel/application/views/restaurant/info.php
+++ hotel/application/views/restaurant/info.php
@@ -2,7 +2,25 @@
   <div class="main-inner">
     <div class="container">
       <div class="span4">
+      	<div class="widget widget-nopad" id="target-1">
+            		<div class="widget-header"> <i class="icon-list-alt"></i>
+             			<h3> Restaraunt</h3>
+            		</div>
+            		<!-- /widget-header -->
+            		<div class="widget-content">
+             			<div class="widget big-stats-container">
+                			<div class="widget-content">
+                 				<h6 class="bigstats">  Here are the weekly featured dishes:</h6>
+                 					Information to be added and formatted
 
+                 				<h6 class="bigstats">  Here is the menu:</h6>
+                 					Information to be added and formatted
+                 				</div>
+                 			</div>
+                 		</div>
+                 	</div>
+                </div>
+                <!-- widget-content -->
       </div>
   	</div>
   </div>
