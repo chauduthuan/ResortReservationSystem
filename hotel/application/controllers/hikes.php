565bcf7c5cec4413451e0b3d8877ec96d87e4a42
Steven Duong <s.duong7@yahoo.com>
2017-11-28 19:28:24 -0800

    modified footer to tailor the text to show our team and the resort name

--- hotel/application/controllers/hikes.php
+++ hotel/application/controllers/hikes.php
@@ -7,10 +7,10 @@ class Hikes extends CI_Controller {
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -  
+	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
@@ -22,26 +22,26 @@ class Hikes extends CI_Controller {
 	{
 		if(!UID)
 			redirect("login");
-	} 
+	}
 
 	public function index()
 	{
 		$this->load->helper('url');
 
 		//$viewdata = array('hikes' => $hikes);
-		
+
 		if(UID != NULL)
 		{
 			$customer = $this->customer_m->get_customer_with_email(UID);
 			$customer_full_name = $customer[0]->customer_firstname." ".$customer[0]->customer_lastname;
 			//echo("<script>console.log('UID: ".json_encode(UID)."');</script>");
 			//echo("<script>console.log('Fullname: ".json_encode($customer_full_name)."');</script>");
-			$data = array('title' => 'Restaurants - DB Hotel Management System', 'page' => 'hikes', 'full_name' => $customer_full_name);
+			$data = array('title' => 'Hikes and Rentals - DB Hotel Management System', 'page' => 'hikes', 'full_name' => $customer_full_name);
 
 		}
 		else
 		{
-			$data = array('title' => 'Restaurants - DB Hotel Management System', 'page' => 'hikes'); 	
+			$data = array('title' => 'Hikes and Rentals - DB Hotel Management System', 'page' => 'hikes'); 	
 		}
 
 		$this->load->view('header', $data);
