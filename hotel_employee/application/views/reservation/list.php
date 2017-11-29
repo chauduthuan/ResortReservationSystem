638a45c3c99949d27f34b591581bec5d6a523626
nibichua <nibichua@gmail.com>
2017-11-29 12:52:47 -0800

    update reservation type

--- hotel_employee/application/views/reservation/list.php
+++ hotel_employee/application/views/reservation/list.php
@@ -19,10 +19,11 @@
 					<label for="customer_TCno">Customer TC no:</label>
 					<input type="text" id="customer_TCno" name="customer_TCno" required readonly value="<?=$customer_TCno?>"/>
 				</div> <!-- /field -->
-
+				
+<!--
 				<div class="field">
 					<label for="reservation_type">Reservation Type:</label>
-					<input type="text" id="reservation_type" name="reservation_type" required readonly value="<?=$rType_name?>"/>
+					<input type="text" id="reservation_type" name="reservation_type" required readonly value="<?=$reservation_type?>"/>
 				 </select>
 				</div> <!-- /field -->
 
@@ -40,6 +41,19 @@
 				<div class="field">
 					<label for="checkout_date">Check-out Date:</label>
 					<input type="date" id="checkout_date" name="checkout_date" required readonly value="<?=$checkout_date?>"/>
+				</div> <!-- /field -->
+				
+				<div class="field">
+					<label for="reservation_type">Reservation Type:</label>
+					<select id="reservation_type" name="reservation_type" >
+					<?
+						foreach ($reservation_types as $k=>$rt) {
+					?>
+						<option value="<?=$rt->reservation_type?>" <? if($k==0) { echo "selected"; } ?>><?=$rt->rType_name?></option>
+					<?
+					}
+					?>
+					</select>
 				</div> <!-- /field -->
 
 				<div class="field">
